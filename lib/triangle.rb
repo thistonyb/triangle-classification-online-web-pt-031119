001e138a66d739afbf25bd4bd3c4e02e934e65f9
Tony Baker <this.tonybaker@gmail.com>
2019-04-12 20:28:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,18 @@ class Triangle
   end
 
   def kind(triangle)
-
-
+    if triange.class != Triangle
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    elsif triangle.is_equilateral?
+      return :equilateral
+    elsif triangle.is_isosceles?
+      return :isosceles
+    else
+      return :scalene
   end
 
   def is_equilateral?
@@ -20,6 +30,8 @@ class Triangle
   def is_isosceles?
     if @length_one == @length_two || @length_one == @lenght_three || @length_two == @lenght_three
   end
+
+  
 
   class TriangleError < StandardError
 
